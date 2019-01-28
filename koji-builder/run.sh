72aa915a0b510d572c3eb0c250cf0196bad20a07
luo fei <luofei163@126.com>
2019-01-28 00:27:46 -0500

    Update run.sh

--- koji-builder/run.sh
+++ koji-builder/run.sh
@@ -3,7 +3,7 @@ cd $NAME
 echo $NAME
 docker build -t $NAME .
 cd ../
-docker run --name $NAME -itd  --privileged --rm $NAME /usr/sbin/init
+docker run --name $NAME -itd  --privileged --rm -v /var/mnt/koji_build_tmp:/var/lib/mock $NAME /usr/sbin/init
 sleep 5
 docker exec -it $NAME /bin/bash -c 'systemctl start kojid && systemctl restart kojid'
 
